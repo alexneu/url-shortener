4ed92634a38210e64638e454b27a9581cb09ff84
Alex Neuhausen <alexneu@gmail.com>
2021-08-24 01:21:25 -0400

    [GB-2] Got slug creation with custom slug, user-specified expiration, working for unauthd users. Need to get auth working, write some tests.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,42 @@
 class ApplicationController < ActionController::API
-  before_action :set_user
+  before_action :authorized
 
-  private
-    def set_user
-      # Presumably fetch the user_id from some kind of session authentication token
-      @user = User.find(user_id)
+  def encode_token(payload)
+    # TODO: Use an actual secret from the SECRET_KEY_BASE environment var
+    JWT.encode(payload, 'secret')
+  end
+ 
+  def auth_header
+    request.headers['Authorization']
+  end
+ 
+  def decoded_token
+    if auth_header
+      # headers: { 'Authorization': 'Bearer <token>' }
+      token = auth_header.split(' ')[1]
+    
+      begin
+      JWT.decode(token, 'secret', true, algorithm: 'HS256')
+  
+      rescue JWT::DecodeError
+        nil
+      end
     end
+  end
+
+  def current_user
+    if decoded_token
+      # decoded_token => [{ "user_id" => 1 }, { "alg" => "HS256" }]
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:create]
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
+  
   resources :urls
   match '*path', to: 'redirect_controller#redirect_from_slug_to_url', via: :all
 end
