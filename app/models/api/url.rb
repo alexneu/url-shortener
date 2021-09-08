3f23d4d92214618753a16317e955ecf08a989ca0
Alex Neuhausen <alexneu@gmail.com>
2021-09-08 17:24:54 -0400

    Added slug cost calculation functions

--- app/models/api/url.rb
+++ app/models/api/url.rb
@@ -21,7 +21,34 @@ class Api::Url < ApplicationRecord
 
   belongs_to :user, index: true, optional: true
 
-  index({ slug: 1 }, { unique: true})
+  index({ slug: 1 }, { unique: true })
+
+  def cost
+    total_cost = 0
+    slug.chars.map { |c| total_cost += self.class.letter_cost(c) }
+    total_cost += repeat_letters_cost
+    total_cost
+  end
+
+  def repeat_letters_cost
+    repeat_cost = 0
+    self.class.characters_incidence_hash(slug).map{ |_k,v| repeat_cost += (v - 1) }
+    repeat_cost
+  end
+
+  def suggested_slug
+    suggested_slug = ''
+    char_map = self.class.characters_incidence_hash(original_url)
+
+    while suggested_slug.length < 4
+      cheapest_char = char_map.keys.find { |c| !self.class.vowel?(c) && !suggested_slug.include?(c) }
+      cheapest_char ||= char_map.keys.find { |c| self.class.vowel?(c) && !suggested_slug.include?(c) }
+      cheapest_char ||= char_map.keys.find { |c| !self.class.vowel?(c) }
+      cheapest_char ||= char_map.keys[0]
+      suggested_slug << cheapest_char
+    end
+    suggested_slug
+  end
 
   class << self
     # Convert expiration from unix epoch timestamp to ruby Time class
@@ -38,6 +65,27 @@ class Api::Url < ApplicationRecord
     def generate_slug(url)
       nonced_url = url + Time.now.strftime("%s")
       Digest::MD5.base64digest(nonced_url)[0..(RANDOM_SLUG_LENGTH-1)].gsub('/', '+').gsub('-', '_')
+    end
+
+    def letter_cost(letter)
+      vowel?(letter) ? 2 : 1
+    end
+  
+    def vowel?(letter)
+      'aeiou'.include?(letter)
+    end
+  
+    def characters_incidence_hash(word)
+      # This will create a hash for goog like { g: 2, o: 2}
+      characters_incidence_hash = {}
+      word.split('').map do |c|
+        if characters_incidence_hash[c].nil?
+          characters_incidence_hash[c] = 1
+        else
+          characters_incidence_hash[c] += 1
+        end
+      end
+      characters_incidence_hash
     end
   end
 end
